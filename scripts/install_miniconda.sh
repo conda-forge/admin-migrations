f0cf2d36ca4fbd95383aac3fb89bc3560356913c
Matthew R Becker <becker.mr@gmail.com>
2020-05-15 11:48:33 -0500

    BUG clone it right

--- scripts/install_miniconda.sh
+++ scripts/install_miniconda.sh
@@ -29,9 +29,3 @@ pip install --no-deps -e .
 
 git config --global user.email "conda-forge-admin@email.com"
 git config --global user.name "conda-forge-admin"
-
-git clone https://${GITHUB_TOKEN}@github.com/conda-forge/feedstock-outputs.git
-export FEEDSTOCK_OUTPUTS_REPO=`pwd`/feedstock-outputs
-pushd feedstock-outputs
-git remote set-url --push origin https://${GITHUB_TOKEN}@github.com/conda-forge/feedstock-outputs.git
-popd
