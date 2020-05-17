2f6cac136c848439b4f77b5b129f5db675f3c180
Matthew R Becker <beckermr@users.noreply.github.com>
2020-05-17 07:06:30 -0500

    Revert "Revert "ENH conda-forge automerge (#33)" (#34)" (#35)
    This reverts commit 6d40c99ad2df097a8e0a3f576ba9c6295855da2e.

--- admin_migrations/migrators/__init__.py
+++ admin_migrations/migrators/__init__.py
@@ -4,3 +4,4 @@ from .automerge_and_botrerun_labels import AutomergeAndBotRerunLabels
 from .appveyor_cleanup import AppveyorDelete
 from .r_automerge import RAutomerge
 from .cfep13_tokens_and_config import CFEP13TokensAndConfig, CFEP13TurnOff
+from .conda_forge_automerge import CondaForgeAutomerge
