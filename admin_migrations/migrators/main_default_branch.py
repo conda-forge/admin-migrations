ba3379dd2d30936cf32bd49a46ed3a61d3df6fea
Matthew R. Becker <becker.mr@gmail.com>
2021-12-16 15:25:34 -0600

    ENH redo migration for actions

--- admin_migrations/migrators/main_default_branch.py
+++ admin_migrations/migrators/main_default_branch.py
@@ -45,17 +45,22 @@ class CondaForgeGHAWithMain(Migrator):
     def migrate(self, feedstock, branch):
         with open(".github/workflows/automerge.yml", "w") as fp:
             fp.write(AUTOMERGE)
-        subprocess.run(
-            ["git", "add", ".github/workflows/automerge.yml"],
-            check=True,
-        )
 
         with open(".github/workflows/webservices.yml", "w") as fp:
             fp.write(WEBSERVICES)
-        subprocess.run(
-            ["git", "add", ".github/workflows/webservices.yml"],
-            check=True,
-        )
 
-        # migration done, make a commit, lots of API calls
-        return True, True, False
+        res = subprocess.run("git diff", shell=True, check=True, capture_output=True)
+        if len(res.stdout + res.stderr) > 0:
+            subprocess.run(
+                ["git", "add", ".github/workflows/automerge.yml"],
+                check=True,
+            )
+            subprocess.run(
+                ["git", "add", ".github/workflows/webservices.yml"],
+                check=True,
+            )
+
+            # migration done, make a commit, lots of API calls
+            return True, True, False
+        else:
+            return True, False, False
