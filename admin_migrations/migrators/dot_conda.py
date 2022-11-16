94ad2b97d1a6bc8ad130972cc768fa443b54f85b
beckermr <becker.mr@gmail.com>
2022-11-16 07:08:41 -0600

    ENH move to .conda

---
+++ admin_migrations/migrators/dot_conda.py
@@ -0,0 +1,57 @@
+import subprocess
+import os
+
+import github
+from ruamel.yaml import YAML
+
+from .base import Migrator
+
+GH = github.Github(os.environ['GITHUB_TOKEN'])
+
+
+def _read_conda_forge_yaml(yaml):
+    if os.path.exists("conda-forge.yml"):
+        with open("conda-forge.yml", "r") as fp:
+            meta_yaml = fp.read()
+
+        if (
+            meta_yaml is None
+            or meta_yaml.strip() == "[]"
+            or meta_yaml.strip() == "[ ]"
+            or len(meta_yaml) == 0
+            or len(meta_yaml.strip()) == 0
+        ):
+            cfg = {}
+        else:
+            cfg = yaml.load(meta_yaml)
+    else:
+        meta_yaml = ""
+        cfg = {}
+
+    return cfg
+
+
+class DotConda(Migrator):
+    def migrate(self, feedstock, branch):
+        repo = GH.get_repo("conda-forge/%s-feedstock" % feedstock)
+        if repo.archived:
+            # migration done, make a commit, lots of API calls
+            return True, False, False
+
+        yaml = YAML()
+        cfg = _read_conda_forge_yaml(yaml)
+        if "conda_pkg_format" not in cfg:
+            cfg["conda_pkg_format"] = "2"
+            with open("conda-forge.yml", "w") as fp:
+                yaml.dump(cfg, fp)
+            subprocess.run(
+                ["git", "add", "conda-forge.yml"],
+                check=True,
+            )
+            print("    updated conda-forge.yml", flush=True)
+            updated_cfy = True
+        else:
+            updated_cfy = False
+
+        # migration done, make a commit, lots of API calls
+        return True, updated_cfy, False
