77c3e62228494b8f17302fc95f1f490303b78149
beckermr <becker.mr@gmail.com>
2024-08-26 09:53:57 -0500

    bump the webservices

--- admin_migrations/migrators/teams_cleanup.py
+++ admin_migrations/migrators/teams_cleanup.py
@@ -2,7 +2,6 @@ import os
 import random
 
 import github
-from conda_smithy.github import configure_github_team
 from ruamel.yaml import YAML
 
 from .base import Migrator
@@ -10,7 +9,16 @@ from .base import Migrator
 GH = github.Github(os.environ['GITHUB_TOKEN'])
 ORG = GH.get_organization("conda-forge")
 
-NUM_DONE = 0
+
+def _get_random_frac():
+    """We do 50 per hour no matter what."""
+    from .._main__ import MAX_MIGRATE
+    if MAX_MIGRATE > 0:
+        frac = 50 / MAX_MIGRATE
+    else:
+        frac = 1
+
+    return frac
 
 
 class DummyMeta(object):
@@ -27,8 +35,6 @@ class TeamsCleanup(Migrator):
     max_processes = 1
 
     def migrate(self, feedstock, branch):
-        global NUM_DONE
-
         repo_name = "%s-feedstock" % feedstock
 
         team_name = feedstock.lower()
@@ -40,38 +46,8 @@ class TeamsCleanup(Migrator):
 
         gh_repo = ORG.get_repo(repo_name)
 
-        # First check for `meta.yaml`
-        if os.path.exists("recipe/meta.yaml"):
-            keep_lines = []
-            skip = True
-            with open("recipe/meta.yaml", "r") as fp:
-                for line in fp.readlines():
-                    if line.startswith("extra:"):
-                        skip = False
-                    if not skip:
-                        keep_lines.append(line)
-        elif os.path.exists("recipe/meta.yml"):
-            # Because `rattler_build` recipes are valid yaml
-            # we can just use the whole file
-            with open("recipe/recipe.yaml") as fp:
-                keep_lines = fp.readlines()
-        else:
-            return False, False, False
-
-        if random.random() < 0.1 and NUM_DONE < 100:
-            meta = DummyMeta("".join(keep_lines))
-            (
-                current_maintainers,
-                prev_maintainers,
-                new_conda_forge_members,
-            ) = configure_github_team(
-                meta,
-                gh_repo,
-                ORG,
-                team_name,
-                remove=True,
-            )
-            NUM_DONE += 1
+        if random.random() < _get_random_frac() or "DEBUG_ADMIN_MIGRATIONS" in os.environ:
+            gh_repo.create_issue(title="@conda-forge-admin, please update team")
 
         # migration done, make a commit, lots of API calls
         return False, False, True
