404fd13fdb2b33093a07b38c2ba1b4b64ae33620
Matthew R. Becker <beckermr@users.noreply.github.com>
2024-08-26 22:24:24 +0200

    feat: turn on teams cleanup (#119)
    * feat: turn on teams cleanup
    
    * fix: some bugs
    
    * feat: do at random and stop if too many done
    
    * test: update test config
    
    * bump the webservices
    
    * style: please the flake8
    
    * fix: wrong module
    
    * fix: weird import issue
    
    * feat: use webserver for this
    
    * feat: raise if fails
    
    * fix: wrong name

---
+++ admin_migrations/defaults.py
@@ -0,0 +1,13 @@
+import datetime
+import os
+
+DEBUG = "DEBUG_ADMIN_MIGRATIONS" in os.environ
+
+if DEBUG:
+    MAX_MIGRATE = 1
+    MAX_SECONDS = 50 * 60
+    MAX_WORKERS = 1
+else:
+    MAX_MIGRATE = 2000
+    MAX_SECONDS = min(50, max(60 - datetime.datetime.now().minute - 6, 0)) * 60
+    MAX_WORKERS = 2
