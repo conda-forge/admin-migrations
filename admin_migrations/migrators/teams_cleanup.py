d2723092e013379e263cc4646c5e3288217024dc
Matthew R. Becker <beckermr@users.noreply.github.com>
2024-10-29 05:53:17 -0500

    feat: print out which teams are updated and use system random (#127)
    * feat: print out which teams are updated
    
    * doc: update PR template
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- admin_migrations/migrators/teams_cleanup.py
+++ admin_migrations/migrators/teams_cleanup.py
@@ -8,6 +8,8 @@ from admin_migrations.defaults import MAX_MIGRATE
 
 from .base import Migrator
 
+RND = random.SystemRandom()
+
 
 def _get_random_frac():
     """We do 50 per hour no matter what."""
@@ -45,16 +47,14 @@ class TeamsCleanup(Migrator):
         ] or team_name.startswith("help-"):
             return
 
-        if (
-            random.random() < _get_random_frac()
-            or "DEBUG_ADMIN_MIGRATIONS" in os.environ
-        ):
+        if RND.random() < _get_random_frac() or "DEBUG_ADMIN_MIGRATIONS" in os.environ:
             rsp = requests.post(
                 "https://conda-forge.herokuapp.com/conda-forge-teams/update",
                 headers={"CF_WEBSERVICES_TOKEN": os.environ["CF_WEBSERVICES_TOKEN"]},
                 json={"feedstock": repo_name},
             )
             rsp.raise_for_status()
+            print("    updated team", flush=True)
 
         # migration done, make a commit, lots of API calls
         return False, False, True
