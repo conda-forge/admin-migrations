e4a79c4d5761a0bd31eecf6d29bbef564cd8dad2
beckermr <becker.mr@gmail.com>
2024-11-01 15:53:11 -0500

    fix: no need to disable

--- admin_migrations/migrators/remove_automerge_and_rerender.py
+++ admin_migrations/migrators/remove_automerge_and_rerender.py
@@ -26,19 +26,19 @@ class RemoveAutomergeAndRerender(Migrator):
                     )
                     make_commit = True
 
-        repo_name = "%s-feedstock" % feedstock
-        gh_repo = ORG.get_repo(repo_name)
-        for fname in ["automerge.yml", "webservices.yml"]:
-            workflow = gh_repo.get_workflow(fname)
-            # /repos/OWNER/REPO/actions/workflows/WORKFLOW_ID/disable
-            url = gh_repo.url + f"/actions/workflows/{workflow.id}/disable"
-            try:
-                gh_repo._requester.requestJsonAndCheck(
-                    "PUT",
-                    url,
-                )
-            except github.GithubException:
-                pass
+        # repo_name = "%s-feedstock" % feedstock
+        # gh_repo = ORG.get_repo(repo_name)
+        # for fname in ["automerge.yml", "webservices.yml"]:
+        #     workflow = gh_repo.get_workflow(fname)
+        #     # /repos/OWNER/REPO/actions/workflows/WORKFLOW_ID/disable
+        #     url = gh_repo.url + f"/actions/workflows/{workflow.id}/disable"
+        #     try:
+        #         gh_repo._requester.requestJsonAndCheck(
+        #             "PUT",
+        #             url,
+        #         )
+        #     except github.GithubException:
+        #         pass
 
         # did it work, commit, made API calls
         return True, make_commit, True
