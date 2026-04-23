c2f057b679e37df2b3e8f7ec13dc576ff0760c71
beckermr <becker.mr@gmail.com>
2026-04-23 04:53:01 -0500

    fix: less io

--- admin_migrations/migrators/r_automerge.py
+++ admin_migrations/migrators/r_automerge.py
@@ -153,15 +153,18 @@ class RAutomerge(Migrator):
     continual = True
 
     def migrate(self, feedstock, branch):
-        print("    r team:", _has_r_team(), flush=True)
-        print("    cran url:", _has_cran_url(), flush=True)
+        has_r_team = _has_r_team() or _has_r_team_rattler_build()
+        has_cran_url = _has_cran_url() or _has_cran_url_rattler_build()
 
         if (
             feedstock.startswith("r-")
             and feedstock != "r-base"
-            and (_has_r_team() or _has_r_team_rattler_build())
-            and (_has_cran_url() or _has_cran_url_rattler_build())
+            and has_r_team
+            and has_cran_url
         ):
+            print("    r team:", has_r_team, flush=True)
+            print("    cran url:", has_cran_url, flush=True)
+
             with Path("conda-forge.yml").open("r") as fp:
                 meta_yaml = fp.read()
 
