90291a154122b0ff118e34aa2147e378d835e94a
beckermr <becker.mr@gmail.com>
2026-04-24 20:49:12 -0500

    fix: put bag debugging

--- admin_migrations/__main__.py
+++ admin_migrations/__main__.py
@@ -444,8 +444,7 @@ def main() -> int:
 
     if DEBUG:
         # set DEBUG_ADMIN_MIGRATIONS in your env to enable this
-        # all_feedstocks = ["cf-autotick-bot-test-package"]
-        all_feedstocks = ["placekey"]
+        all_feedstocks = ["cf-autotick-bot-test-package"]
         feedstocks["feedstocks"] = all_feedstocks
         feedstocks["current_feedstock"] = "a"
         assert feedstocks["feedstocks"][0] > feedstocks["current_feedstock"]
